c6a31b7f39df5b6cbe658c94a65635cc20e6fc04
Mwangimark <markcosmars2000@gmail.com>
2025-11-07 11:59:03 +0300

    updated pages url to reflect that in components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import Team from "@/components/Team";
@@ -8,11 +10,13 @@ import Team from "@/components/Team";
 export default function Home() {
   return (
     <main className="min-h-screen">
+      <Header />
       <Hero />
       <About />
       <Services />
       <Team/>
       <Contact />
+      <Footer />
     </main>
   );
 }
